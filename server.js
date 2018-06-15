319232dc629dde1c1fe2cbe7fcdb37992cf614d4
Adam Heath <doogie@brainfood.com>
2018-06-15 15:26:07 -0500

    Massive data model update, for redux rewrite.

--- server.js
+++ server.js
@@ -35,6 +35,58 @@ async function dbPoolWorker(res, handler) {
   }
 }
 
+async function updateTags(client, iiifId, tags) {
+  const query = `
+WITH iiif_object AS (
+  SELECT iiif_type_id, external_id FROM iiif WHERE iiif_id = $1
+), tag_list AS (
+  SELECT ROW_NUMBER() OVER() AS sequence_num, tag.tag FROM (SELECT UNNEST($2::text[]) AS tag) tag
+), tag_id AS (
+  INSERT INTO iiif_tags (iiif_type_id, tag) SELECT iiif_object.iiif_type_id, tag_list.tag FROM iiif_object CROSS JOIN tag_list
+  ON CONFLICT (iiif_type_id, tag) DO UPDATE SET tag = EXCLUDED.tag
+  RETURNING *
+), override_id AS (
+  INSERT INTO iiif_overrides (external_id) SELECT iiif_object.external_id FROM iiif_object
+  ON CONFLICT (external_id) DO UPDATE SET external_id = EXCLUDED.external_id
+  RETURNING iiif_override_id
+), delete_ignore AS (
+  DELETE FROM iiif_overrides_tags USING tag_id WHERE iiif_overrides_tags.iiif_tag_id NOT IN (tag_id.iiif_tag_id)
+)
+INSERT INTO iiif_overrides_tags (iiif_override_id, iiif_tag_id, sequence_num)
+SELECT
+  c.iiif_override_id, b.iiif_tag_id, a.sequence_num
+FROM
+  tag_list a JOIN tag_id b ON
+    a.tag = b.tag
+  CROSS JOIN override_id c
+ON CONFLICT (iiif_override_id, iiif_tag_id) DO UPDATE SET sequence_num = EXCLUDED.sequence_num
+`
+  return await client.query(query, [iiifId, tags])
+}
+
+async function getTags(client, iiifId) {
+  const query = `
+SELECT
+  b.tag
+FROM
+  iiif a JOIN iiif_tags b ON
+    a.iiif_type_id = b.iiif_type_id
+  JOIN iiif_overrides c ON
+    a.external_id = c.external_id
+  JOIN iiif_overrides_tags d ON
+    c.iiif_override_id = d.iiif_override_id
+    AND
+    b.iiif_tag_id = d.iiif_tag_id
+WHERE
+  iiif_id = $1
+ORDER BY
+  d.sequence_num
+`
+  const tagsResult = await client.query(query, [iiifId])
+  return tagsResult.rows.map(row => row.tag)
+}
+
+
 const app = express()
 app.use(cors())
 
@@ -55,14 +107,43 @@ app.get('/collection/:collectionId', (req, res) => {
     const collectionResult = await client.query("SELECT iiif_id, label FROM iiif WHERE iiif_type_id = $1 AND iiif_id = $2", [type, collectionId])
     const firstRow = collectionResult.rows[0]
     const assocResult = await client.query("SELECT a.iiif_id_to, a.iiif_assoc_type_id, b.label, b.iiif_type_id FROM iiif_assoc a JOIN iiif b ON a.iiif_id_to = b.iiif_id WHERE a.iiif_id_from = $1 ORDER BY a.sequence_num, b.label", [collectionId])
+    const tags = await getTags(client, collectionId)
     return {
       id: firstRow.iiif_id,
       label: firstRow.label,
-      members: assocResult.rows.map(row => {
+      manifests: assocResult.rows.map(row => {
         return {id: row.iiif_id_to, type: row.iiif_assoc_type_id, label: row.label, type: row.iiif_type_id}
       }),
       type,
+      tags,
     }
+  })
+})
+
+app.post('/collection/:collectionId', jsonParser, (req, res) => {
+  dbPoolWorker(res, async client => {
+    const {collectionId} = req.params
+    const {body: {notes, tags}} = req
+    const query = `
+WITH collection_external_id AS (
+  SELECT
+    external_id
+  FROM
+    collection
+  WHERE
+    iiif_id = $1
+)
+INSERT INTO iiif_overrides
+  (external_id, notes)
+  SELECT
+    collection_external_id.external_id, $2
+  FROM collection_external_id
+
+  ON CONFLICT (external_id) DO UPDATE SET notes = $2
+`
+    const insertUpdateResult = await client.query(query, [collectionId, notes])
+    await updateTags(client, collectionId, tags)
+    return {ok: true}
   })
 })
 
@@ -75,6 +156,7 @@ app.get('/manifest/:manifestId', (req, res) => {
   dbPoolWorker(res, async client => {
     const {manifestId} = req.params
     const manifestResult = await client.query("SELECT * FROM manifest WHERE iiif_id = $1", [manifestId])
+    const tags = await getTags(client, manifestId)
     const firstRow = manifestResult.rows[0]
     return {
       id: firstRow.iiif_id,
@@ -85,7 +167,35 @@ app.get('/manifest/:manifestId', (req, res) => {
       logo: firstRow.logo,
       type: firstRow.iiif_type_id,
       viewingHint: firstRow.viewingHint,
+      tags,
     }
+  })
+})
+
+app.post('/manifest/:manifestId', jsonParser, (req, res) => {
+  dbPoolWorker(res, async client => {
+    const {manifestId} = req.params
+    const {body: {notes, tags}} = req
+    const query = `
+WITH manifest_external_id AS (
+  SELECT
+    external_id
+  FROM
+    manifest
+  WHERE
+    iiif_id = $1
+)
+INSERT INTO iiif_overrides
+  (external_id, notes)
+  SELECT
+    manifest_external_id.external_id, $2
+  FROM manifest_external_id
+
+  ON CONFLICT (external_id) DO UPDATE SET notes = $2
+`
+    const insertUpdateResult = await client.query(query, [manifestId, notes])
+    await updateTags(client, manifestId, tags)
+    return {ok: true}
   })
 })
 
@@ -147,8 +257,27 @@ ORDER BY
   })
 })
 
-async function updateTags(client, iiifId, tags) {
-}
+app.get('/range/:rangeId', (req, res) => {
+  dbPoolWorker(res, async client => {
+    const {rangeId} = req.params
+    const rangeResult = await client.query("SELECT * FROM range WHERE iiif_id = $1", [rangeId])
+    const rangeOverrideResult = await client.query("SELECT * FROM range_overrides WHERE iiif_id = $1", [rangeId])
+    const tags = await getTags(client, rangeId)
+    const firstRow = rangeResult.rows[0]
+    const firstOverrideRow = rangeOverrideResult.rows[0] || {}
+    return {
+      id: firstRow.iiif_id,
+      label: firstRow.label,
+      type: firstRow.iiif_type_id,
+      viewingHint: firstRow.viewingHint,
+      notes: firstOverrideRow.notes,
+      fovAngle: firstOverrideRow.fov_angle,
+      fovDepth: firstOverrideRow.fov_depth,
+      fovOrientation: firstOverrideRow.fov_orientation,
+      tags,
+    }
+  })
+})
 
 app.post('/range/:rangeId', jsonParser, (req, res) => {
   dbPoolWorker(res, async client => {
@@ -184,102 +313,6 @@ INSERT INTO iiif_range_overrides
     const insertUpdateResult = await client.query(query, [rangeId, notes, fovAngle, fovDepth, fovOrientation])
     await updateTags(client, rangeId, tags)
     return {ok: true}
-  })
-})
-
-app.post('/canvas/:canvasId', jsonParser, (req, res) => {
-  dbPoolWorker(res, async client => {
-    const {canvasId} = req.params
-    console.log(req.body)
-    const {body: {notes, tags = [], exclude = false, hole = false}} = req
-    const query = `
-WITH canvas_external_id AS (
-  SELECT
-    external_id
-  FROM
-    canvas
-  WHERE
-    iiif_id = $1
-), override_id AS (
-  INSERT INTO iiif_overrides
-    (external_id, notes)
-    SELECT
-      canvas_external_id.external_id, $2
-    FROM canvas_external_id
-
-  ON CONFLICT (external_id) DO UPDATE SET notes = $2
-  RETURNING iiif_override_id
-)
-INSERT INTO iiif_canvas_overrides
-  (iiif_override_id, exclude, hole)
-  SELECT
-    override_id.iiif_override_id, $3, $4,
-  FROM override_id
-
-  ON CONFLICT (iiif_override_id) DO UPDATE SET (exclude, hole) = ROW($3, $4)
-`
-    const insertUpdateResult = await client.query(query, [canvasId, notes, exclude, hole])
-    await updateTags(client, canvasId, tags)
-    return {ok: true}
-  })
-})
-
-app.post('/canvas/:canvasId/point/:sourceId', jsonParser, (req, res) => {
-  dbPoolWorker(res, async client => {
-    const {canvasId, sourceId} = req.params
-    console.log(req.body)
-    const {body: {priority, point}} = req
-    const query = `
-WITH canvas_external_id AS (
-  SELECT
-    external_id
-  FROM
-    canvas
-  WHERE
-    iiif_id = $1
-), override_id AS (
-  INSERT INTO iiif_overrides
-    (external_id)
-    SELECT
-      canvas_external_id.external_id
-    FROM canvas_external_id
-
-    ON CONFLICT (external_id) DO UPDATE SET external_id = EXCLUDED.external_id
-    RETURNING iiif_override_id
-)
-INSERT INTO iiif_canvas_point_overrides
-  (iiif_override_id, iiif_canvas_override_source_id, priority, point)
-  SELECT
-    override_id.iiif_override_id, $2, $3, ST_SetSRID(ST_GeomFromGeoJSON($4), 4326)
-  FROM override_id
-
-  ON CONFLICT (iiif_override_id, iiif_canvas_override_source_id)
-  DO UPDATE SET (priority, point) = ROW($3, CASE WHEN $4 IS NOT NULL THEN ST_SetSRID(ST_GeomFromGeoJSON($4), 4326) ELSE NULL END)
-`
-    const insertUpdateResult = await client.query(query, [canvasId, sourceId, priority, point])
-    return {ok: true}
-  })
-})
-
-app.get('/range/:rangeId', (req, res) => {
-  dbPoolWorker(res, async client => {
-    const {rangeId} = req.params
-    const rangeResult = await client.query("SELECT * FROM range WHERE iiif_id = $1", [rangeId])
-    const rangeOverrideResult = await client.query("SELECT * FROM range_overrides WHERE iiif_id = $1", [rangeId])
-    console.log('rangeOverrideResult', rangeOverrideResult)
-    const firstRow = rangeResult.rows[0]
-    const firstOverrideRow = rangeOverrideResult.rows[0] || {}
-    console.log('firstOverrideRow', firstOverrideRow)
-    return {
-      id: firstRow.iiif_id,
-      label: firstRow.label,
-      type: firstRow.iiif_type_id,
-      viewingHint: firstRow.viewingHint,
-      notes: firstOverrideRow.notes,
-      fovAngle: firstOverrideRow.fov_angle,
-      fovDepth: firstOverrideRow.fov_depth,
-      fovOrientation: firstOverrideRow.fov_orientation,
-    }
   })
 })
 
@@ -364,6 +397,134 @@ ORDER BY
       }
       return ({id, point: JSON.parse(point), overrides, ...row})
     })
+  })
+})
+
+app.get('/canvas/:canvasId', (req, res) => {
+  dbPoolWorker(res, async client => {
+    const {canvasId} = req.params
+    const canvasResult = await client.query("SELECT * FROM canvas WHERE iiif_id = $1", [canvasId])
+    const canvasOverrideResult = await client.query("SELECT * FROM canvas_overrides WHERE iiif_id = $1", [canvasId])
+    const tags = await getTags(client, canvasId)
+    const firstRow = canvasResult.rows[0]
+    const firstOverrideRow = canvasOverrideResult.rows[0] || {}
+    return {
+      id: firstRow.iiif_id,
+      label: firstRow.label,
+      type: firstRow.iiif_type_id,
+      format: firstRow.format,
+      height: firstRow.height,
+      image: firstRow.image,
+      thumbnail: firstRow.thumbnail,
+      width: firstRow.width,
+
+      notes: firstOverrideRow.notes,
+      exclude: firstOverrideRow.exclude,
+      hole: firstOverrideRow.hole,
+      tags,
+      /*
+      fovAngle: firstOverrideRow.fov_angle,
+      fovDepth: firstOverrideRow.fov_depth,
+      fovOrientation: firstOverrideRow.fov_orientation,
+      */
+    }
+  })
+})
+
+app.post('/canvas/:canvasId', jsonParser, (req, res) => {
+  dbPoolWorker(res, async client => {
+    const {canvasId} = req.params
+    const {body: {notes, tags = [], exclude = false, hole = false}} = req
+    const query = `
+WITH canvas_external_id AS (
+  SELECT
+    external_id
+  FROM
+    canvas
+  WHERE
+    iiif_id = $1
+), override_id AS (
+  INSERT INTO iiif_overrides
+    (external_id, notes)
+    SELECT
+      canvas_external_id.external_id, $2
+    FROM canvas_external_id
+
+  ON CONFLICT (external_id) DO UPDATE SET notes = $2
+  RETURNING iiif_override_id
+)
+INSERT INTO iiif_canvas_overrides
+  (iiif_override_id, exclude, hole)
+  SELECT
+    override_id.iiif_override_id, $3, $4
+  FROM override_id
+
+  ON CONFLICT (iiif_override_id) DO UPDATE SET (exclude, hole) = ROW($3, $4)
+`
+    const insertUpdateResult = await client.query(query, [canvasId, notes, exclude, hole])
+    await updateTags(client, canvasId, tags)
+    return {ok: true}
+  })
+})
+
+app.post('/canvas/:canvasId/point/:sourceId', jsonParser, (req, res) => {
+  dbPoolWorker(res, async client => {
+    const {canvasId, sourceId} = req.params
+    console.log(req.body)
+    const {body: {priority, point}} = req
+    const query = `
+WITH canvas_external_id AS (
+  SELECT
+    external_id
+  FROM
+    canvas
+  WHERE
+    iiif_id = $1
+), override_id AS (
+  INSERT INTO iiif_overrides
+    (external_id)
+    SELECT
+      canvas_external_id.external_id
+    FROM canvas_external_id
+
+    ON CONFLICT (external_id) DO UPDATE SET external_id = EXCLUDED.external_id
+    RETURNING iiif_override_id
+)
+INSERT INTO iiif_canvas_point_overrides
+  (iiif_override_id, iiif_canvas_override_source_id, priority, point)
+  SELECT
+    override_id.iiif_override_id, $2, $3, ST_SetSRID(ST_GeomFromGeoJSON($4), 4326)
+  FROM override_id
+
+  ON CONFLICT (iiif_override_id, iiif_canvas_override_source_id)
+  DO UPDATE SET (priority, point) = ROW($3, CASE WHEN $4 IS NOT NULL THEN ST_SetSRID(ST_GeomFromGeoJSON($4), 4326) ELSE NULL END)
+`
+    const insertUpdateResult = await client.query(query, [canvasId, sourceId, priority, point])
+    return {ok: true}
+  })
+})
+
+app.delete('/canvas/:canvasId/point/:sourceId', jsonParser, (req, res) => {
+  dbPoolWorker(res, async client => {
+    const {canvasId, sourceId} = req.params
+    const query = `
+WITH override_id AS (
+  SELECT
+    iiif_override_id
+  FROM
+    canvas_point_overrides
+  WHERE
+    iiif_id = $1
+)
+DELETE FROM iiif_canvas_point_overrides
+  USING override_id
+  WHERE
+    iiif_canvas_point_overrides.iiif_override_id = override_id.iiif_override_id
+    AND
+    iiif_canvas_override_source_id = $2
+`
+    const deleteResult = await client.query(query, [canvasId, sourceId])
+    return {ok: true}
   })
 })
 
