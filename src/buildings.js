9a4c1341b6c9f871891325da293c3a2f37e8cd35
Adam Heath <doogie@brainfood.com>
2018-09-21 16:00:16 -0500

    Return the most recent taxdata for a building, if it exists.

--- src/buildings.js
+++ src/buildings.js
@@ -2,20 +2,33 @@
 
 export async function getBuildings(client, ...ogcFids) {
   let query = `
-SELECT DISTINCT
-  ogc_fid, code, bld_id, height, elev, area, source, date_, ain, shape_leng, shape_area,
+SELECT
+  a.ogc_fid, a.code, a.bld_id, a.height, a.elev, a.area, a.source, a.date_, a.ain, a.shape_leng, a.shape_area,
+  row_to_json(b) AS taxdata,
   ST_AsGeoJSON(wkb_geometry) AS geojson
 FROM
-  lariac_buildings
+  lariac_buildings a LEFT JOIN LATERAL (
+		WITH max AS (
+			SELECT max(roll_year) AS roll_year FROM taxdata WHERE a.ain::numeric = taxdata.ain
+		)
+		SELECT
+			*
+		FROM
+			taxdata
+		WHERE
+			a.ain::numeric = ain
+			AND
+			roll_year = (SELECT roll_year FROM max)
+	) b ON
+			a.ain::numeric = b.ain
 WHERE
 `.replace(/[\t\r\n ]+/g, ' ')
   if (!ogcFids.length) {
     return null
   }
-  query += ` ogc_fid IN (${ogcFids.map((id, index) => `$${index + 1}`).join(', ')})`
+  query += ` a.ogc_fid IN (${ogcFids.map((id, index) => `$${index + 1}`).join(', ')})`
   const result = await client.query(query, ogcFids)
   return result.rows.map(({ogc_fid: id, geojson, ...rest}) => {
     return {...rest, id, geojson: geojson ? JSON.parse(geojson) : null}
   })
 }
-
