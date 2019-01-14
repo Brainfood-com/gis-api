c516f78edc57435586d99e591196da017127f168
Adam Heath <doogie@brainfood.com>
2019-01-14 14:03:15 -0600

    Reformat one query to be multi-line.

--- src/iiif/manifest.js
+++ src/iiif/manifest.js
@@ -56,7 +56,29 @@ export async function getOverrides(client, iiifOverrideId) {
 }
 
 export async function getStructures(client, manifestId) {
-  const manifestRangesResult = await client.query("SELECT a.iiif_id_to AS range_id, b.external_id, b.iiif_type_id, b.label, c.viewing_hint, d.values FROM iiif_assoc a JOIN iiif b ON a.iiif_id_to = b.iiif_id AND a.iiif_assoc_type_id = 'sc:Range' JOIN iiif_range c ON b.iiif_id = c.iiif_id LEFT JOIN iiif_values d ON b.iiif_id = d.iiif_id WHERE a.iiif_id_from = $1 ORDER BY a.sequence_num, b.label", [manifestId])
+  const manifestRangesResult = await client.query(`
+SELECT
+  a.iiif_id_to AS range_id,
+  b.external_id,
+  b.iiif_type_id,
+  b.label,
+  c.viewing_hint,
+  d.values
+FROM
+  iiif_assoc a JOIN iiif b ON
+    a.iiif_id_to = b.iiif_id
+    AND
+    a.iiif_assoc_type_id = 'sc:Range'
+  JOIN iiif_range c ON
+    b.iiif_id = c.iiif_id
+  LEFT JOIN iiif_values d ON
+    b.iiif_id = d.iiif_id
+WHERE
+  a.iiif_id_from = $1
+ORDER BY
+  a.sequence_num,
+  b.label
+`, [manifestId])
   const manifestRangeMembersResult = await client.query(`
 WITH has_point_override AS (
   SELECT
